4929494bff84b24284d4673fc060a18aba270131
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2023-12-05 20:36:50 -0800

    monitor: add replay monitoring script

---
+++ monitor.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+WATCHME=$1
+REPLAY_DIR=$2
+KERCHUNK=$3
+
+inotifywait -m "$WATCHME" -e create |
+    while read -r directory action file; do
+        echo "New shit just got made: $file"
+
+        case "$file" in
+            (*.mkv)
+            if [ "$KERCHUNK" -eq "yes" ]; then
+                KERCHUNK=''
+
+                sleep 1
+
+                continue
+            fi
+                 
+            if [ 3500 -gt $(du "$file" | awk '{ print $1 }') ]; then
+                mv "$file" "$REPLAY_DIR"
+                
+                move_error=$?
+
+                echo "Moved '$file', error code $move_error"
+            else
+                echo "I don't think thats a replay."
+            fi
+
+            ;;
+        esac
+    done
