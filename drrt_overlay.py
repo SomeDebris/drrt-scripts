e61e33aededfdd338a59e700d83642523baae29c
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2024-01-10 23:19:22 -0800

    drrt overlay: create file for working on a script that writes for obs

---
+++ drrt_overlay.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+DRRT DATASHEET CONNECTION
+Connects to the DRRT Datasheet and does stuff.
+"""
+
+import argparse
+import csv
+import gzip
+import json
+import os
+import re
+import shutil
+import subprocess
+import sys
+
+from drrt_common import VERSION, TOURNAMENT_DIRECTORY, SCRIPT_DIR, print_err, wait_yn
+from log_reader import get_ship_list
+
+"""
+function that creates two text files when called with 
+a match number: 
+    RED ALLIANCE NAME
+    BLUE ALLIANCE NAME
+"""
+def print_ships_at_match( number_match ):
+    filepath_selected_schedule = os.path.join( SCRIPT_DIR, 'selected_schedule.csv' )
+
+    if ( not os.path.exists( filepath_selected_schedule ) ):
+        print_err( f"drrt_overlay: I can't find the selected_schedule.csv file!" )
+
+    with open( filepath_selected_schedule ) as file_schedule:
+        schedule_reader = csv.reader( file_schedule )
+
+        for row in schedule_reader:
+            print( row )
+
+
+
