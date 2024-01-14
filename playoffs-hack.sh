55e8075eda253a96f12d1308922dff133adb5276
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2024-01-14 11:43:23 -0800

    hack: display what its printing to my stuff

--- playoffs-hack.sh
+++ playoffs-hack.sh
@@ -11,8 +11,11 @@ while true; do
     RIGHT_LARGE="./playoffs_hacked/${RIGHT}_large.txt"
     RIGHT_SMALL="./playoffs_hacked/${RIGHT}_small.txt"
 
+    echo "--- LEFT SPAWNING ALLIANCE ---"
     cat "$LEFT_LARGE" > ./red_NEXT.txt
-    cat "$LEFT_SMALL" > ./red_NEXT_SMALL.txt
+    cat "$LEFT_SMALL" | tee ./red_NEXT_SMALL.txt
+    echo "--- RIGHT SPAWNING ALLIANCE ---"
     cat "$RIGHT_LARGE" > ./blue_NEXT.txt
-    cat "$RIGHT_SMALL" > ./blue_NEXT_SMALL.txt
+    cat "$RIGHT_SMALL" | tee ./blue_NEXT_SMALL.txt
+    echo ""
 done
