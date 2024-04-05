b12e52ed490612e7275f75bced104230e8a3ae4a
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2024-04-04 23:02:58 -0700

    Begin mapping out what I want to write for the tcl'd DRRT scripts

--- drrt_scheduler.tcl
+++ drrt_scheduler.tcl
@@ -1,3 +1,18 @@
 #!/usr/bin/env tclsh
 
+package require json
+
 puts "Heyy this worked!"
+
+# What does this script need to accomplish in order to truly function as the 
+# DRRT Scheduler?
+# - Create correct folder structure. This should likely be in a specified 
+#   location
+# - Reference a match schedule file and index the array based on it
+# - alliance assembler function
+
+proc saveFleetToFile {filename fleet} {
+}
+
+proc makeShipsIntoFleet {ships} {
+}
