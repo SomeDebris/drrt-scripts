58f4b1beaf84e519ca9b9aa24ca3ca0cd816e261
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2024-04-06 10:07:55 -0700

    Copy a piece of code from stackoverflow (from user: Jerry) that turns Tcl dicts to json

--- drrt_common.tcl
+++ drrt_common.tcl
@@ -5,10 +5,43 @@ package require json::write
 
 # Converts a dict object to a json object
 # Taken from the tcl docs
-proc dict2json {dict_to_encode} {
-    ::json::write object {*}[dict map {k v} $dict_to_encode {
-        set v [::json::write string $v]
-    }]
+# proc dict2json {dict_to_encode} {
+#     set accumulator {}
+#     ::json::write object {*}[dict map {k v} $dict_to_encode {
+#         set v [::json::write string $v]
+#     }]
+# }
+
+# Lets try to understand this person's code!
+proc dict2json {dict_to_encode spec {indent false}} {
+    ::json::write::indented $indent
+
+    set accumulator [dict create]
+
+    dict for {field type_info} $spec {
+        if {![dict exists $dict_to_encode $field]} {continue}
+
+        lassign $type_info type meta
+
+        set value [dict get $dict_to_encode $field]
+        switch $type {
+            object {
+                set value [dict2json $value $meta $indent]
+            }
+            array {
+                set value [list2jsonarray $value {*}$meta]
+            }
+            string {
+                set value [::json::write string $value]
+            }
+            bare {}
+            default {
+                return -code error "Type \"$type\" is not known!"
+            }
+        }
+        dict set accumulator $field $value
+    }
+    return [::json::write object {*}$accumulator]
 }
 
 proc waitYN {prompt_string} {
