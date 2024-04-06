2c95f3b82006c1b197b1f86e540cd8d0842ead01
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2024-04-06 11:38:00 -0700

    No need for dict2json with rl_json library installed

--- drrt_common.tcl
+++ drrt_common.tcl
@@ -1,7 +1,6 @@
 # Common commands that we all should use
 
-package require json
-package require json::write
+package require rl_json
 
 # Converts a dict object to a json object
 # Taken from the tcl docs
@@ -11,69 +10,6 @@ package require json::write
 #         set v [::json::write string $v]
 #     }]
 # }
-
-# Lets try to understand this person's code!
-proc dict2json {dict_to_encode spec {indent false}} {
-    ::json::write::indented $indent
-
-    set accumulator [dict create]
-
-    dict for {field type_info} $spec {
-        if {![dict exists $dict_to_encode $field]} {continue}
-
-        lassign $type_info type meta
-
-        set value [dict get $dict_to_encode $field]
-        switch $type {
-            object {
-                set value [dict2json $value $meta $indent]
-            }
-            array {
-                set value [list2jsonarray $value {*}$meta]
-            }
-            string {
-                set value [::json::write string $value]
-            }
-            bare {}
-            default {
-                return -code error "Type \"$type\" is not known!"
-            }
-        }
-        dict set accumulator $field $value
-    }
-    return [::json::write object {*}$accumulator]
-}
-
-proc list2jsonarray {list_to_encode type {meta {}}} {
-    set accumulator [list]
-
-    switch $type {
-        object {
-            foreach element $list_to_encode {
-                lappend accumulator [dict2json $element $meta false]
-            }
-        }
-        array {
-            lassign $meta subtype submeta
-            foreach element $list_to_encode {
-                lappend accumulator \
-                        [list2jsonarray $element $subtype $submeta]
-            }
-        }
-        string {
-            foreach element $list_to_encode {
-                lappend accumulator [::json::write string $element]
-            }
-        }
-        bare {
-            set accumulator $list_to_encode
-        }
-        default {
-            return -code error "Invalid array element type: $type"
-        }
-    }
-    return [::json::write array {*}$accumulator]
-}
 
 proc waitYN {prompt_string} {
     while {true} {
