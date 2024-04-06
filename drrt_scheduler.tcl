85f919878e3fc83c788028c45d111e193215c6f1
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2024-04-06 09:46:01 -0700

    Define some gosh danged functions for parsing ship files as Tcl datatypes!

--- drrt_scheduler.tcl
+++ drrt_scheduler.tcl
@@ -10,20 +10,48 @@ puts "Heyy this worked!"
 #   location
 # - Reference a match schedule file and index the array based on it
 # - alliance assembler function
+proc checkShipFileExtension {filename} {
+    switch -glob -nocase -- "$filename" {
+        *.json {
+            return {json}
+        }
+        *.json.gz {
+            return {jsongz}
+        }
+        *.lua {
+            return {lua}
+        }
+        *.lua.gz {
+            return {luagz}
+        }
+        default {
+            error "File extension of $filename should be legal for a ship file, but wasn't!"
+        }
+    }
+}
+
 proc makeShipDictFromFile {filename} {
+    set shipfiletype [checkShipFileExtension $filename]
+
     set filehandle [open "$filename" r]
     set filecontents [read $filehandle]
     close $filehandle
 
-    switch -glob -nocase -- "$filename" {
-        *.json {
+    switch $shipfiletype {
+        json {
             return -code 0 [makeShipDictFromJson filecontents]
         }
-        *.lua {
+        jsongz {
+            error "gzipped JSON files cannot be parsed yet. Please gunzip them."
+        }
+        lua {
             error "Lua ship files cannot be parsed yet. Please re-export your ships as JSON."
         }
+        luagz {
+            error "gzipped Lua ship files cannot be parsed yet. Please re-export your ships as JSON."
+        }
         default {
-            error "File extension of $filename should be either .lua or .json, but was neither!"
+            error "Ship file \"$filename\" type found to be \"$shipfiletype\", but \"$shipfiletype\" has no dict creation procedure defined."
         }
     }
 }
