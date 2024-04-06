5c6c7fb890131104beda11a5456d3f036ec66752
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2024-04-06 09:27:45 -0700

    Create a simple command for getting a dict from json. aim to make one for lua too

--- drrt_scheduler.tcl
+++ drrt_scheduler.tcl
@@ -11,6 +11,22 @@ puts "Heyy this worked!"
 # - Reference a match schedule file and index the array based on it
 # - alliance assembler function
 proc makeShipDictFromFile {filename} {
+    set filehandle [open "$filename" r]
+    set filecontents [read $filehandle]
+    close $filehandle
+
+    switch -glob -nocase -- "$filename" {
+        *.json {
+            makeShipDictFromJson 
+    }
+}
+
+proc makeShipDictFromJson {varname} {
+    upvar $varname jsonstring
+
+    set shipdict {[::json::json2dict $jsonstring]}
+
+    return $shipdict
 }
 
 proc saveFleetToFile {filename fleet} {
