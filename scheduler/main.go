291d9238b74d6c4568a00e7bf7f90dbd5f7eb32a
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2025-01-14 18:10:53 -0800

    begin translating it to go. uh oh, I don't have enough time

--- scheduler/main.go
+++ scheduler/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
-    "path/filepath"
 	"drrt-scripts/lib"
 	"flag"
-    "sort"
+	"fmt"
+	"os"
+	"path/filepath"
+	"sort"
+    "log"
+    "json"
 )
 
 func get_inspected_ship_paths(dir string) ([]string, error) {
@@ -51,16 +52,36 @@ func main() {
             if os.IsExist(err) {
                 fmt.Printf("Directory '%s' already exists.\n", dir)
             } else {
-                fmt.Printf("Error making directory '%s'.\n", dir)
+                fmt.Printf("Error making directory '%s': %s\n", dir, err)
+                os.Exit(1)
             }
         }
     }
 
+    ships_directory := filepath.Join(*drrt_directory_arg, "Ships")
+    quals_directory := filepath.Join(*drrt_directory_arg, "Qualifications")
+    stags_directory := filepath.Join(*drrt_directory_arg, "Staging")
+    playf_directory := filepath.Join(*drrt_directory_arg, "Playoffs")
+    scrpt_directory := filepath.Join(*drrt_directory_arg, "drrt-scripts")
+
     // Empty the contents of the Qualifications directory
-    err := lib.Remove_directory_contents(filepath.Join(*drrt_directory_arg, "Qualifications"))
+    err := lib.Remove_directory_contents(quals_directory)
     if err != nil {
-        fmt.Printf("cannot remove contents of the 'Qualifications' directory.\n")
+        log.Fatalf("Cannot remove contents of Qualifications directory: %s\n", err)
     }
 
-        
+    ships, err := get_inspected_ship_paths(ships_directory) 
+    if err != nil {
+        log.Fatalf("Cannot get inspected ship paths: %s\n", err)
+    }
+    
+    fmt.Printf("Found %s ship files!\n", len(ships))
+
+    if len(ships) < (*ships_per_alliance_arg * 2) {
+        log.Fatalf("%d is lesser than the minimum number of ships (%d).\n",
+                   len(ships), *ships_per_alliance_arg)
+    }
+
+    sch_in_filename := fmt.Sprintf("%d_%dv%d.csv", len(ships), *ships_per_alliance_arg, *ships_per_alliance_arg)
+    sch_in_filepath := filepath.Join(scrpt_directory, "schedules", "out", sch_in_filename)
 }
