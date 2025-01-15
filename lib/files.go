59692608a47d82c13b2a970186e96891e4807c2c
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2025-01-14 16:00:46 -0800

    Add all new go files. maybe I'll have binary drrt shit!

---
+++ lib/files.go
@@ -0,0 +1,45 @@
+package lib
+
+import (
+    "os"
+    "path/filepath"
+)
+
+func path_exists(path string) bool {
+    _, err := os.Stat(path)
+
+    if err != nil {
+        if os.IsNotExist(err) {
+            return false
+        } else {
+            // It may not exist!
+            return false
+        }
+    }
+
+    return true
+}
+
+func remove_contents(directory string) error {
+    d, err := os.Open(directory)
+    if err != nil {
+        return err
+    }
+
+    defer d.Close()
+
+    names, err := d.Readdirnames(-1)
+
+    if err != nil {
+        return err
+    }
+
+    for _, name := range names {
+        err = os.RemoveAll(filepath.Join(directory, name))
+        if err != nil {
+            return err
+        }
+    }
+
+    return nil
+}
