f414a2ff1257fc70c335b4d01f9464824ad18c71
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2025-07-19 19:52:14 -0700

    check if the ship file path is json

--- lib/constants.go
+++ lib/constants.go
@@ -1,43 +1,22 @@
 package lib
 
-import (
-	"encoding/json"
-)
+// import (
+// 	"encoding/json"
+// )
 
-type CommandData struct {
-	Flags   json.RawMessage `json:"flags,omitempty"`
-	Faction int             `json:"faction,omitempty"`
+var RED_ALLIANCE_TEMPLATE Fleet = Fleet{
+	[]Ship{},
+	0xBAA01E,
+	0x681818,
+	0x000000,
+	100,
+	"The Red Alliance",
 }
-
-type Block struct {
-	Id      any          `json:"ident"`
-	Offset  [2]float64   `json:"offset"`
-	Angle   float64      `json:"angle"`
-	Command *CommandData `json:"command,omitempty"`
-	BindingId int `json:"bindingId,omitempty"`
-}
-
-type ShipData struct {
-	Name   string          `json:"name"`
-	Author string          `json:"author"`
-	Color0 json.RawMessage `json:"color0,omitempty"`
-	Color1 json.RawMessage `json:"color1,omitempty"`
-	Color2 json.RawMessage `json:"color2,omitempty"`
-	Wgroup [4]int          `json:"wgroup,omitempty"`
-}
-
-type Ship struct {
-	Angle    float64    `json:"angle,omitempty"`
-	Position [2]float64 `json:"position,omitempty"`
-	Data     ShipData   `json:"data"`
-	Blocks   []Block    `json:"blocks"`
-}
-
-type Fleet struct {
-	Blueprints []Ship          `json:"blueprints"`
-	Color0     json.RawMessage `json:"color0,omitempty"`
-	Color1     json.RawMessage `json:"color1,omitempty"`
-	Color2     json.RawMessage `json:"color2,omitempty"`
-	Faction    int             `json:"faction"`
-	Name       string          `json:"name"`
+var BLUE_ALLIANCE_TEMPLATE Fleet = Fleet{
+	[]Ship{},
+	0x0AA879,
+	0x222D84,
+	0x000000,
+	101,
+	"The Blue Alliance",
 }
