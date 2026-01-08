edbbb779cf2d9f2d40717368ce563f3d56710535
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2026-01-08 00:11:22 -0800

    lib: create file with function for reading and blocking on named pipes

---
+++ lib/unixNamedPipe.go
@@ -0,0 +1,41 @@
+package lib
+
+import (
+	"bufio"
+	"errors"
+	"io"
+	"log/slog"
+	"os"
+)
+
+const (
+	DRRT_MLOG_SIGNAL_PIPE_PATH = `/tmp/drrt_mlog_signal_pipe`
+)
+
+// Make function for creating a named pipe in the /tmp directory with a global filename
+
+// make function for returning value from named pipe
+func ReadDRRTMlogPipe(path string) (string, error) {
+	slog.Info("Trying to open mlog signal pipe.", "path", path)
+	pipe, err := os.OpenFile(path, os.O_RDONLY, os.ModeNamedPipe|0600)
+	if err != nil {
+		return "", err
+	}
+	defer pipe.Close()
+
+	var line []byte
+
+	reader := bufio.NewReader(pipe)
+	for {
+		line, err = reader.ReadBytes('\n')
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				slog.Info("Done reading from pipe.")
+				break
+			}
+			slog.Error("Failed to read from pipe.", "err", err)
+			return "", err
+		}
+	}
+	return string(line), nil
+}
